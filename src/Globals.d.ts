27cd5bfd6e12e75b98c66b2c0eced8b0091978f7
Henrique Jensen <henriquejensen@hotmail.com>
2022-04-24 23:22:58 -0300

    adds my photo

--- src/Globals.d.ts
+++ src/Globals.d.ts
@@ -1,3 +1,4 @@
 declare module "*.module.css";
 declare module "*.module.scss";
+declare module "*.png";
 // and so on for whatever flavor of css you're using
--- src/components/LogoContainer.tsx
+++ src/components/LogoContainer.tsx
@@ -1,14 +1,10 @@
-// import Logo from "../images/my.svg";
-// import Background from "../images/background.svg";
-import OriginalBackground from "../images/logo.svg";
+import Logo from "../images/me.svg";
 import styles from "./LogoContainer.module.css";
 
 export default function LogoContainer() {
   return (
     <section className={styles.container}>
-      {/* <img src={Logo} alt="" />
-      <img src={Background} alt="" className={styles.background} /> */}
-      <img src={OriginalBackground} alt="" />
+      <img src={Logo} alt="" />
     </section>
   )
 }
