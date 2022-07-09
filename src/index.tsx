1bcbdee9b6a32c8651d699f1ec0b7ead81ffdb3a
Henrique Jensen <henriquejensen@hotmail.com>
2022-07-09 15:23:21 -0300

    atualiza para React 18

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,16 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import React from 'react';
+import { createRoot } from 'react-dom/client';
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
-ReactDOM.render(
+const container = document.getElementById('root');
+if (container === null) throw new Error('Root container missing in index.html')
+const root = createRoot(container);
+root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-  document.getElementById("root")
-);
+  </React.StrictMode>
+)
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
